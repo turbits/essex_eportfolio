2eab74018193d0dd96437636eb03717e5dd52ff6
turbits <turbits@protonmail.com>
2022-12-11 16:40:30 -0700

    fix comment block

--- pages/module2/assignment3/unit10/program.py
+++ pages/module2/assignment3/unit10/program.py
@@ -35,3 +35,4 @@ The correct answers are import my_module and from my_module import MyClass.
 While from my_module import * is technically valid Python code, programmers are encourage to avoid the wildcard when importing from modules.
 
 When importing, you always start with the the module name. Either import the entire module, import my_module or indicate the module then import the item, from my_module import MyClass. Do not start with the item name.
+"""
